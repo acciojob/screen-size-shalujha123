c3c96d32064c94acd718e7155a5685fbbc0c44a8
Shalu <88194182+shalujha123@users.noreply.github.com>
2025-02-09 21:45:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+let w = document.documentElement.clientWidth
+let h = document.documentElement.clientHeight
+function getResize(){
+	document.getElementById("sizeInfo").innerHTML = "<h1>Width: "+w+" and"+" Height: "+h
+}
